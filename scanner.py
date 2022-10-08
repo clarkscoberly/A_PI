5509668c2b508ccfe27ed2dbb0dae47449812b48
Grant Holley <97457524+gh0lree21@users.noreply.github.com>
2022-10-07 23:45:30 -0600

    Update scanner.py
    scanner working stuff

--- scanner.py
+++ scanner.py
@@ -1,1 +1,22 @@
 
+from scapy.all import ARP, Ether, srp
+
+
+# target_ip = "10.244.149.69"
+target_ip = "10.9.160.115"
+arp = ARP(pdst=target_ip)
+
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+
+packet = ether/arp
+result = srp(packet, timeout=3)[0]
+
+clients = []
+for sent, received in result:
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+    print("Available devices in the network:")
+    print("IP" + " "*18+"MAC")
+    for client in clients:
+        print("{:16}    {}".format(client['ip'], client['mac']))
+        
