31007feb52ef9a8751be23c4a320613b2c0c643f
clarkscoberly <cob18002@byui.edu>
2022-10-08 07:30:03 -0600

    structured database to recieve and give back data

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ from getpass import getpass
 from mysql.connector import connect, Error
 import scapy.all as scapy
 import argparse
+from datetime import date
 
 # The following are the locations of where to store the data and where the data is within a list to be
 # Pulled back out
@@ -14,8 +15,8 @@ DATE_AND_TIME = 5
 
 
 # user = A_PI
-# pass = T3@ms_ap1
-# host = 
+# pass = T3@ms_AP1
+# host = 0.0.0.0:5555
 
 class Db:
 
@@ -36,10 +37,12 @@ class Db:
         except Error as e:
             print(e)
 
-    def update_table(self, formatted_data):
+    def update_table(self, data):
         """
         Get formatted information and then send it to the database.
         """
+        
+        formatted_data = self.format_data(data)
 
         update_to_send = formatted_data
         insert_data_query = """
@@ -48,48 +51,50 @@ class Db:
         VALUES ( %s, %s, %s, %s, %s, %s )
         """
 
-        ###################################################################################################
+        ###########################################################################################################
         # The following is an example of the kind of formatted data that should be contained within formatted data.
-        reviewers_records = [
-            ("Chaitanya", "Baweja"),
-            ("Mary", "Cooper"),
-            ("John", "Wayne"),
-            ("Thomas", "Stoneman")
-        ]
-        ###################################################################################################
+        #
+        # reviewers_records = [
+        #     ("Chaitanya", "Baweja"),
+        #     ("Mary", "Cooper"),
+        #     ("John", "Wayne"),
+        #     ("Thomas", "Stoneman")
+        # ]
+        #
+        ###########################################################################################################
 
         with self.connection.cursor() as cursor:
             cursor.executemany(insert_data_query, formatted_data)
             self.connection.commit()
 
-    def get_information(self, data): # TODO DATA IS NOT WRITTEN YET
+    def format_data(self):
         """
-        Gets info from the scanner or from main after the data is no longer in .csv form 
+        Format data retrieved by scanner to be put into
         """
-        self.send_info(data)
         pass
 
-    def populate_information(self):
+    # def get_information(self, data): # TODO DATA IS NOT WRITTEN YET
+    #     """
+    #     Gets info from the scanner or from main after the data is no longer in .csv form 
+    #     """
+    #     self.send_info(data)
+    #     pass
+
+    # TODO get query Selection
+
+    def populate_information(self, query_selection):
         """
-        Takes information from get_information and prepares it to be sent to GUI
+        Prepares info from a query, returns result to GUI
         """
-        #                            TODO THIS STATEMENT MAY ME INCORRECT
-        # get_data_query = f"SELECT * FROM data WHERE start_date < "
-        get_data_query = f"SELECT * FROM data"
+        current_date = date.today()
+
+        # TODO THIS MIGHT ALSO SELECT THE AUTOINCREMENT KEY IN WHICH CASE ALL THE NUMS ARE WRONG
+        # where date(created_at) = CURDATE() - interval 7 day | <?
+        get_data_query = f"SELECT * FROM data WHERE start_date = {current_date} - interval {query_selection} day"
         with self.connection.cursor() as cursor:
             cursor.execute(get_data_query)
             result = cursor.fetchall()
 
-
-            return self._format_info()
-            
-            # TODO send result to gui for population
-
-    def _format_info(self):
-        """
-        Takes a list of lists and formats them accordingly then returns a list of lists
-        """
-        
-        pass
+            return result
 
 db = Db()
